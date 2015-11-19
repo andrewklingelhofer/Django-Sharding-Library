8b406e434433d215771e72173a6754cb99a3daa4
Andrew Klingelhofer <andrewcklingelhofer@gmail.com>
2015-11-19 11:03:59 -0800

    Added comments

--- db/shards/routers.py
+++ db/shards/routers.py
@@ -1,11 +1,20 @@
+"""
+    Routes sharded data to correct database based on sharding id
+"""
 
-
-class ShardedRouter(objects):
+class ShardingRouter(objects):
     
     def db_for_read(self, model, **hints):
-        
+        """ 
+            returns the database based on which database the info
+            should go to
+        """
 
     def db_for_write(self, model, **hints):
+        """ 
+            Calls db_for_read so it knows which database to write
+            the info to
+        """
 
     def allow_relation(self, obj1, obj2, **hints):
         """ Not sure if we need this """
