f541dd261b8e9571ca18d14ab87295fe2608b2f5
Andrew Klingelhofer <andrewcklingelhofer@gmail.com>
2015-12-05 10:55:51 -0800

    Some work on models, needs a lot more love

---
+++ db/shards/models.py
@@ -0,0 +1,51 @@
+# Model for user in Django App
+
+# Imports (Not sure what's needed yet)
+
+from django.db.models import Model
+from django.db.models.fields.related import ForeignKey
+
+class ShardedForeignKey(ForeignKey):
+    def __init--(self, ... , **kwargs):
+
+class LogicalShardOptions():
+    def __init__(self, physical_shard, shard_id):
+        self.physical_shard = physical_shard
+        self.shard_id = shard_id
+        self.name = None # Name of particular shard
+        self.model = None # Type of model this shard contains
+
+    @property
+    def is_logical_shard(self):
+        # This is a logical shard
+        return True
+
+    @property
+    def is_physical_shard(self):
+        # This is not a physical shard
+        return False
+    
+    @property
+    def physical_shard_key(self):
+        return self.physical_shard. 
+
+    def get_database(self):
+        # Return database of logical shard
+        physical_shard = self.physical_shard
+
+
+class PhysicalShardOptions():
+    def __init__(self, options, logical_shards=[]):
+        self.options = options # Options of the physical shard
+        self.logical_shards = logical_shards # Logical Shard info for cluster
+        self.model = None # Type of model this physical shard may use?
+        self.name = None # Name of cluster
+
+    @property
+    def is_logical_shard(self):
+        return False
+
+    @property
+    def is_physical_shard(self):
+        return True
+
