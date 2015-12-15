b1846087c92dc8ce0bcc2821b6834f8b4a269c10
ack8 <andrewcklingelhofer@gmail.com>
2015-12-15 16:12:48 -0500

    Updated routers for new models syntax

--- db/shards/routers.py
+++ db/shards/routers.py
@@ -1,25 +1,27 @@
 # Routes sharded data to correct database based on sharding id
+from models import logical_to_physical, shard
 
-class ShardingRouter(objects):
-    
-    def db_for_read(self, model, **hints):
+class ShardingRouter(object):
+
+    def get_database(self, shard_key):
+        return logical_to_physical(shard(shard_key))
+
+    def db_for_read_or_write(self, model, **hints):
         # Returns the database based on which database the info
         # should go to
-        sharding_function = self.getattr(model, '_sharding_function', None)
-        if sharding_info:
-            # If sharding_info exists, make sure it's a logical shard
-            if sharding_info.is_logical_shard:
-                # This means it can return the database it's located in
-                return sharding_info.get_database()
-            else:
-                raise ValueError('Not a logical shard, can\'t be queried')
+        shard_key = self.getattr(model, '_shard', None)
+        if shard_key:
+            return get_database(shard_key)
+        else:
+            raise ValueError('shard_key does not exist, can\'t be queried')
         
+    def db_for_read(self, model, **hints):
+        return self.db_for_read_or_write(model, **hints)
 
     def db_for_write(self, model, **hints):
         # Calls db_for_read so it knows which database to write
         # the info to
-        hints['is_write'] = True
-        return self.db_for_read(model, **hints)
+        return self.db_for_read_or_write(model, **hints)
 
     def allow_relation(self, obj1, obj2, **hints):
         # Not sure if we need this
