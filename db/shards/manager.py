8773d8a5382feb399ea641865b5f6e1e92d2dd7c
Rene Tianco <rt1230@nyu.edu>
2015-12-14 23:52:18 -0500

    modified sharded manager
    Sharded manager now takes queries that pass either an instance or shard_key as a keyword

--- db/shards/manager.py
+++ db/shards/manager.py
@@ -5,11 +5,7 @@ from helpers import find_shard_key
 
 class ShardedManager(models.Manager):
 
-    def __init__(self):
-        super(ShardedManager, self).__init__()
-        # self.queryset_class = ShardedQuerySet
-
-    def _get_queryset(self, instance):
+    def _get_queryset_with_instance(self, instance):
         hints = self._hints
         if hints:
             hints['shard_key'] = find_shard_key(instance)
@@ -17,11 +13,35 @@ class ShardedManager(models.Manager):
             hints = {'shard_key': find_shard_key(instance)}
         return self._queryset_class(model=self.model, hints=hints)
 
-    def all(self, instance):
-        return getattr(self._get_queryset(instance), 'all')()
+    def _get_queryset_with_shardkey(self, shard_key):
+        hints = self._hints
+        if hints:
+            hints['shard_key'] = shard_key
+        else:
+            hints = {'shard_key': shard_key}
+        return self._queryset_class(model=self.model, hints=hints)
 
-    def filter(self, instance, *args, **kwargs):
-        return getattr(self._get_queryset(instance), 'filter')(*args, **kwargs)
+    def _query_with_shardkey_or_instance(self, func_name, *args, **kwargs):
+        instance = kwargs.get('instance', None)
+        shard_key = kwargs.get('shard_key', None)
+        if instance:
+            del kwargs['instance']
+            return getattr(self._get_queryset_with_instance(instance), func_name)(*args, **kwargs)
+        elif shard_key:
+            del kwargs['shard_key']
+            return getattr(self._get_queryset_with_shardkey(shard_key), func_name)(*args, **kwargs)
+        else:
+            raise Exception('You must pass either a shard_key or instance as a keyword')
 
-    def get(self, instance, *args, **kwargs):
-        return getattr(self._get_queryset(instance), 'get')(*args, **kwargs)
+    def all(self, *args, **kwargs):
+        return self._query_with_shardkey_or_instance('all', *args, **kwargs)
+
+    def filter(self, *args, **kwargs):
+        return self._query_with_shardkey_or_instance('filter', *args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        shard_key = kwargs.get('shard_key', None)
+        if shard_key:
+            return getattr(self._get_queryset_with_shardkey(shard_key), 'get')(*args, **kwargs)
+        else:
+            raise Exception('Get must be passed a shard_key as a keyword')
